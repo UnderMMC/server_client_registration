1438fa8a98ffa56a12a03520958b91db63074983
UnderMMC <mrak1tin@yandex.ru>
2024-08-11 17:53:08 +0300

    Add files via upload

--- server.go
+++ server.go
@@ -1,61 +1,59 @@
 package main
 
 import (
-	"database/sql"
+	//"database/sql"
 	"encoding/json"
-	"log"
 	"net/http"
 
 	_ "github.com/lib/pq"
 )
 
 type RegistrData struct {
-	Login    string
-	Password string
+	Name     string `json:"name"`
+	Password string `json:"password"`
 }
 
 func PersonalDataHandler(w http.ResponseWriter, r *http.Request) {
+	//w.WriteHeader(http.StatusOK)
 	data := RegistrData{}
 	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	//err = DataBaseRecording(data.Name, data.Password)
+	/*if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}*/
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
 }
 
-func DataBaseRecording() {
-	connStr := "user=postgres dbname=postgres password=4125 host=localhost sslmode=disable"
+/*func DataBaseRecording(name, password string) error {
+	connStr := "user=postgres dbname=postgres password=postgres host=localhost sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatalf("Ошибка при открытии базы данных: %v", err)
+		return err
 	}
 	defer db.Close() // Закрываем базу данных в конце функции
 
 	// Проверяем соединение
 	err = db.Ping()
 	if err != nil {
-		log.Fatalf("Ошибка при подключении к базе данных: %v", err)
+		return err
 	}
 
-	// Создание таблицы
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username TEXT, password TEXT)")
+	_, err = db.Exec("INSERT INTO users (name, password) VALUES ($1, $2)", name, password)
 	if err != nil {
-		log.Fatalf("Ошибка при создании таблицы: %v", err)
+		return err
 	}
-
-	_, err = db.Exec("INSERT INTO users (username, password) VALUES ($1, $2)", "user1", "password1")
-	if err != nil {
-		log.Fatalf("Ошибка при вставке данных: %v", err)
-	}
-
-	log.Println("Данные успешно вставлены в таблицу users")
-}
+	return nil
+}*/
 
 func main() {
+
 	http.HandleFunc("/api/registr", PersonalDataHandler)
-	DataBaseRecording()
 	http.ListenAndServe(":8080", nil)
 }
